dc020f317bedc130a5db08ad83cd42c9725199fb
Tasawar Hussain <leonine7218@gmail.com>
2020-09-27 23:26:11 +0500

    Update exception error messages to be more verbose

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -6,11 +6,16 @@ module ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
+      erorr_message = "Couldn't find #{e.model} with 'id = #{e.id}'"
+      json_response({ message:  erorr_message }, :not_found)
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
       json_response({ message: e.message }, :unprocessable_entity)
+    end
+
+    rescue_from ActionController::ParameterMissing do |_e|
+      json_response({ message: 'Parameters missing: provide atleast one field' }, :unprocessable_entity)
     end
   end
 end
